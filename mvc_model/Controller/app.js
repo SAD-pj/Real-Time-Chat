e0ee60e626b6c6710ffd16ab6088e0d65a35f4ff
ZMYLYH <125202100+zmylyh@users.noreply.github.com>
2024-11-18 19:24:22 +0800

    同步

--- mvc_model/Controller/app.js
+++ mvc_model/Controller/app.js
@@ -1,25 +1,6 @@
-// const express = require("express")
-//
-// const middleware = require('./middleware')
-//
-// const router = require('./router')
-//
-// const app = express()
-// // 中间件
-// app.use(middleware)
-//
-// // 路由
-// app.use('/api', router)
-//
-// // 身份认证失败中间件
-// const errAuth = require('./middleware/errAuth')
-// app.use(errAuth)
-//
-// // 启动服务
-// const server = app.listen(5323, function () {
-//   console.log(`App running at local http://localhost:5323`)
-// })
-//
-// // 聊天socket
-// const socket = require('./utils/socket')
-// socket(server)
+const express = require("express")
+const middleware = require('./middleware')
+const app = express()
+const server = app.listen(3000, function () {
+  console.log(`App running at local http://localhost:3000`)
+})
